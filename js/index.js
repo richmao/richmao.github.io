0d11388230b78fff109ba4446ee7cc3ec69fe30b
Richard Mao <richmao@gmail.com>
2018-04-25 22:48:04 -0700

    lazy loading

--- js/index.js
+++ js/index.js
@@ -29,3 +29,12 @@ function toggleFunction() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = function () {
+        img.removeAttribute('data-src');
+    };
+});
+    
+    
