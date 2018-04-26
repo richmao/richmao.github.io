a6c63e535d11305a307eec4600fa9ba9b3a792fe
Richard Mao <richmao@gmail.com>
2018-04-25 23:03:12 -0700

    removed lazy loading

--- js/index.js
+++ js/index.js
@@ -30,13 +30,6 @@ function toggleFunction() {
     }
 }
 
-[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
-    img.setAttribute('src', img.getAttribute('data-src'));
-    img.onload = function () {
-        img.removeAttribute('data-src');
-    };
-    console.log("Done");
-    
-});
+
     
     
